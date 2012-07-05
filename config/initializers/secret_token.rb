6abdf8ee66298c73a48dfbde87ff04dbe7130d71
Raife Stokes <raifestokes@gmail.com>
2012-07-05 21:08:56 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePm::Application.config.secret_token = '47cd1524f3d8ba08d0e9602b1e2fd25200a8ee2fc47cb1ccdce65b74c043ad0dd57b97d7fccbea844199cd57e9a83b1e5af8abc3ae58ed12b6a7c2843cdbdc4e'
