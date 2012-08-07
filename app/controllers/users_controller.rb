034fc460be042c55bfc52b52369a81dd7f8912b6
Raife Stokes <raifestokes@gmail.com>
2012-08-07 18:38:56 +1000

    updating users settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,31 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def show
-  @user=User.find(params[:id])
+    @user=User.find(params[:id])
   end
   def create
-     @user = User.new(params[:user])
-     if @user.save
-         flash[:success] = "Welcome to the Sample App!"
-       redirect_to @user
-     else
-       # debugger
-       render 'new'
-     end
-   end
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      # debugger
+      render 'new'
+    end
+  end
+  def edit
+    @user=User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # handle a successful update here
+      flash[:success]="Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "User Pages" do
     it { should have_selector("h1", text: "Sign up") }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
-  
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -17,27 +17,54 @@ describe "User Pages" do
   end
   describe "signup" do
 
-     before { visit signup_path }
+    before { visit signup_path }
 
-     let(:submit) { "Create my account" }
+    let(:submit) { "Create my account" }
 
-     describe "with invalid information" do
-       it "should not create a user" do
-         expect { click_button submit }.not_to change(User, :count)
-       end
-     end
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
 
-     describe "with valid information" do
-       before do
-         fill_in "Name",         with: "Example User"
-         fill_in "Email",        with: "user@example.com"
-         fill_in "Password",     with: "foobar"
-         fill_in "Confirmation", with: "foobar"
-       end
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
 
-       it "should create a user" do
-         expect { click_button submit }.to change(User, :count).by(1)
-       end
-     end
-   end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+    end
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",         with: new_name
+        fill_in "Email",        with: new_email
+        fill_in "Password",     with: user.password
+        fill_in "Confirmation", with: user.password
+        click_button "Save changes"
+      end
+      it { should have_selector('title', text: new_name) }
+    it { should have_selector('div.alert.alert-success') }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+  end
 end
